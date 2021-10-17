8c395c57b2938c6564d6bc18c3318ae176ef4a65
jungjee27 <78446584+jungjee27@users.noreply.github.com>
2021-10-16 21:58:59 -0400

    Update BlogService.java

--- src/main/java/levvel/io/service/BlogService.java
+++ src/main/java/levvel/io/service/BlogService.java
@@ -1,10 +1,17 @@
 package levvel.io.service;
 
 import levvel.io.model.Blog;
+import levvel.io.model.Comment;
+
 
 public interface BlogService {
 
     void addBlog(Blog blog);
 
     Blog getBlog(String id);
+    
+    void addComment(Comment comment, String id);
+
+    Comment[] getComments(String id);
+    
 }
