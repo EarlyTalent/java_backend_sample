0ad23dbd45eddb097968b16da4e7fe8447808482
Arham4 <arhamjsiddiqui@gmail.com>
2022-10-02 10:21:31 -0500

    Add unit tests for getting blog post

--- src/test/java/levvel/io/BlogServiceTests.java
+++ src/test/java/levvel/io/BlogServiceTests.java
@@ -1,6 +1,7 @@
 package levvel.io;
 
 import levvel.io.data.BlogRepository;
+import levvel.io.exception.BlogNotExistException;
 import levvel.io.model.Blog;
 import levvel.io.service.BlogService;
 import levvel.io.service.BlogServiceImpl;
@@ -15,7 +16,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class BlogServiceTests {
@@ -41,17 +42,45 @@ class BlogServiceTests {
 	@Test
 	public void Should_AddPostToMockList_When_AddBlog() {
 		List<Blog> mockBlogsList = new ArrayList<>();
-		Mockito.when(blogRepository.save(Mockito.any())).thenAnswer(invocation -> {
-			Blog blog = invocation.getArgument(0);
-			mockBlogsList.add(blog);
-			return blog;
-		});
+		prepareMockBlogRepository(mockBlogsList);
 
 		Blog blog = createTestBlog();
 		blogService.addBlog(blog);
 
 		assertEquals(mockBlogsList.size(), 1);
 		assertEquals(mockBlogsList.get(0).getId(), "123abc");
+	}
+
+	@Test
+	public void Should_GetPostFromMockList_When_GetBlog() throws BlogNotExistException {
+		List<Blog> mockBlogsList = new ArrayList<>(List.of(createTestBlog()));
+		prepareMockBlogRepository(mockBlogsList);
+
+		Blog blog = blogService.getBlog("123abc");
+
+		assertNotNull(blog);
+		assertEquals(blog.getAuthor(), "Levvel");
+	}
+
+	@Test
+	public void Should_Fail_When_GettingBlogWithUnknownId() {
+		List<Blog> mockBlogsList = new ArrayList<>(List.of(createTestBlog()));
+		prepareMockBlogRepository(mockBlogsList);
+
+		assertThrows(BlogNotExistException.class, () -> blogService.getBlog("diffId123"));
+	}
+
+	private void prepareMockBlogRepository(List<Blog> mockBlogsList) {
+		Mockito.when(blogRepository.save(Mockito.any())).thenAnswer(invocation -> {
+			Blog blog = invocation.getArgument(0);
+			mockBlogsList.add(blog);
+			return blog;
+		});
+
+		Mockito.when(blogRepository.findById(Mockito.any()))
+				.thenAnswer(invocation -> mockBlogsList.stream()
+						.filter(b -> b.getId().equals(invocation.getArgument(0)))
+						.findFirst());
 	}
 
 	private Blog createTestBlog() {
