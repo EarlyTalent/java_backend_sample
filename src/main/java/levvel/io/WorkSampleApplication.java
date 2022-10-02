fa8250ee397eb38eabc5ba68bf1930f0fa2b9c2f
Arham4 <arhamjsiddiqui@gmail.com>
2022-10-02 11:08:24 -0500

    Replace tabs with spaces

--- src/main/java/levvel/io/WorkSampleApplication.java
+++ src/main/java/levvel/io/WorkSampleApplication.java
@@ -8,8 +8,8 @@ import org.springframework.data.mongodb.config.EnableMongoAuditing;
 @EnableMongoAuditing
 public class WorkSampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WorkSampleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WorkSampleApplication.class, args);
+    }
 
 }
--- src/test/java/levvel/io/BlogServiceTests.java
+++ src/test/java/levvel/io/BlogServiceTests.java
@@ -21,74 +21,74 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 public final class BlogServiceTests {
 
-	@Mock
-	private BlogRepository blogRepository;
+    @Mock
+    private BlogRepository blogRepository;
 
-	private BlogService blogService;
+    private BlogService blogService;
 
-	private AutoCloseable closeable;
+    private AutoCloseable closeable;
 
-	@BeforeEach
-	public void setup() {
-		closeable = MockitoAnnotations.openMocks(this);
-		blogService = new BlogServiceImpl(blogRepository);
-	}
+    @BeforeEach
+    public void setup() {
+        closeable = MockitoAnnotations.openMocks(this);
+        blogService = new BlogServiceImpl(blogRepository);
+    }
 
-	@AfterEach
-	void closeService() throws Exception {
-		closeable.close();
-	}
+    @AfterEach
+    void closeService() throws Exception {
+        closeable.close();
+    }
 
-	@Test
-	public void Should_AddPostToMockList_When_AddBlog() {
-		List<Blog> mockBlogsList = new ArrayList<>();
-		prepareMockBlogRepository(mockBlogsList);
+    @Test
+    public void Should_AddPostToMockList_When_AddBlog() {
+        List<Blog> mockBlogsList = new ArrayList<>();
+        prepareMockBlogRepository(mockBlogsList);
 
-		Blog blog = createTestBlog();
-		blogService.addBlog(blog);
+        Blog blog = createTestBlog();
+        blogService.addBlog(blog);
 
-		assertEquals(mockBlogsList.size(), 1);
-		assertEquals(mockBlogsList.get(0).getId(), "123abc");
-	}
+        assertEquals(mockBlogsList.size(), 1);
+        assertEquals(mockBlogsList.get(0).getId(), "123abc");
+    }
 
-	@Test
-	public void Should_GetPostFromMockList_When_GetBlog() throws BlogNotExistException {
-		List<Blog> mockBlogsList = new ArrayList<>(List.of(createTestBlog()));
-		prepareMockBlogRepository(mockBlogsList);
+    @Test
+    public void Should_GetPostFromMockList_When_GetBlog() throws BlogNotExistException {
+        List<Blog> mockBlogsList = new ArrayList<>(List.of(createTestBlog()));
+        prepareMockBlogRepository(mockBlogsList);
 
-		Blog blog = blogService.getBlog("123abc");
+        Blog blog = blogService.getBlog("123abc");
 
-		assertNotNull(blog);
-		assertEquals(blog.getAuthor(), "Levvel");
-	}
+        assertNotNull(blog);
+        assertEquals(blog.getAuthor(), "Levvel");
+    }
 
-	@Test
-	public void Should_Fail_When_GettingBlogWithUnknownId() {
-		List<Blog> mockBlogsList = new ArrayList<>(List.of(createTestBlog()));
-		prepareMockBlogRepository(mockBlogsList);
+    @Test
+    public void Should_Fail_When_GettingBlogWithUnknownId() {
+        List<Blog> mockBlogsList = new ArrayList<>(List.of(createTestBlog()));
+        prepareMockBlogRepository(mockBlogsList);
 
-		assertThrows(BlogNotExistException.class, () -> blogService.getBlog("diffId123"));
-	}
+        assertThrows(BlogNotExistException.class, () -> blogService.getBlog("diffId123"));
+    }
 
-	private void prepareMockBlogRepository(List<Blog> mockBlogsList) {
-		Mockito.when(blogRepository.save(Mockito.any())).thenAnswer(invocation -> {
-			Blog blog = invocation.getArgument(0);
-			mockBlogsList.add(blog);
-			return blog;
-		});
+    private void prepareMockBlogRepository(List<Blog> mockBlogsList) {
+        Mockito.when(blogRepository.save(Mockito.any())).thenAnswer(invocation -> {
+            Blog blog = invocation.getArgument(0);
+            mockBlogsList.add(blog);
+            return blog;
+        });
 
-		Mockito.when(blogRepository.findById(Mockito.any()))
-				.thenAnswer(invocation -> mockBlogsList.stream()
-						.filter(b -> b.getId().equals(invocation.getArgument(0)))
-						.findFirst());
-	}
+        Mockito.when(blogRepository.findById(Mockito.any()))
+                .thenAnswer(invocation -> mockBlogsList.stream()
+                        .filter(b -> b.getId().equals(invocation.getArgument(0)))
+                        .findFirst());
+    }
 
-	private Blog createTestBlog() {
-		Blog blog = new Blog();
-		blog.setId("123abc");
-		blog.setAuthor("Levvel");
-		blog.setText("Body text");
-		return blog;
-	}
+    private Blog createTestBlog() {
+        Blog blog = new Blog();
+        blog.setId("123abc");
+        blog.setAuthor("Levvel");
+        blog.setText("Body text");
+        return blog;
+    }
 
 }
