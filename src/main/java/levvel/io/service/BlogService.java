f9b608a3c293c3ef7c86cd7c7a6f4a7b62553200
djlee19 <59945116+djlee19@users.noreply.github.com>
2022-10-08 23:28:03 -0400

    Update BlogService.java

--- src/main/java/levvel/io/service/BlogService.java
+++ src/main/java/levvel/io/service/BlogService.java
@@ -1,10 +1,16 @@
 package levvel.io.service;
 
+import java.util.ArrayList;
 import levvel.io.model.Blog;
+import levvel.io.model.Comment;
 
 public interface BlogService {
 
     void addBlog(Blog blog);
 
     Blog getBlog(String id);
+
+    void putComment(String id, Comment comment);
+
+    ArrayList<Comment> getComments(String id);
 }
