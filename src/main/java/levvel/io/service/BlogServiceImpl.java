647dc8413624e44dadff5d3a72cdfdd664ca161d
djlee19 <59945116+djlee19@users.noreply.github.com>
2022-10-08 23:28:19 -0400

    Update BlogServiceImpl.java

--- src/main/java/levvel/io/service/BlogServiceImpl.java
+++ src/main/java/levvel/io/service/BlogServiceImpl.java
@@ -2,6 +2,8 @@ package levvel.io.service;
 
 import levvel.io.data.BlogRepository;
 import levvel.io.model.Blog;
+import levvel.io.model.Comment;
+import java.util.ArrayList;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,11 +15,23 @@ public class BlogServiceImpl implements BlogService {
 
     @Override
     public void addBlog(Blog blog) {
-        blogRepository.save(blog);
+        blogRepository.save(blog); // saves blog into database
     }
 
     @Override
     public Blog getBlog(String id) {
         return blogRepository.findById(id).orElseGet(null);
+    }
+
+    // my code here: 
+    public void putComment(String id, Comment comment) {
+        Blog blog = blogRepository.findById(id).orElseGet(null);
+        blog.addComment(comment);
+        blogRepository.save(blog);
+    }
+
+    public ArrayList<Comment> getComments(String id) {
+        Blog blog = blogRepository.findById(id).orElseGet(null);
+        return blog.getAllComments();
     }
 }
