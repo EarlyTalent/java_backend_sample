2a5a76070e30464b0a8fbf474ebd91383c432be2
djlee19 <59945116+djlee19@users.noreply.github.com>
2022-10-08 23:26:25 -0400

    Update Blog.java

--- src/main/java/levvel/io/model/Blog.java
+++ src/main/java/levvel/io/model/Blog.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
+import java.util.ArrayList;
 
 import java.time.LocalDateTime;
 
@@ -22,4 +23,15 @@ public class Blog {
     String author;
     String title;
     String text;
+    
+    public void addComment(Comment comment) {
+        comments.add(comment);
+    }
+
+    public ArrayList<Comment> getAllComments() {
+        return comments;
+    }
+    // array list of comments to store comments
+    ArrayList<Comment> comments= new ArrayList<Comment>();
 }
+
